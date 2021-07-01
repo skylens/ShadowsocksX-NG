462035c5311050b14ac94d0b288b00a44202f83d
skylens <skylens116@outlook.com>
2021-07-02 02:05:57 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 brew install mbedtls tree
-xcodebuild clean -workspace /Users/runner/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -configuration Release
-xcodebuild archive -workspace /Users/runner/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -archivePath /Users/runner/archive/ShadowsocksX-NG.xcarchive
+xcodebuild clean -workspace /Users/runner/work/ShadowsocksX-NG/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -configuration Release
+xcodebuild archive -workspace /Users/runner/work/ShadowsocksX-NG/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -archivePath /Users/runner/archive/ShadowsocksX-NG.xcarchive
 tree /Users/runner/archive/ShadowsocksX-NG.xcarchive/Products/Applications
