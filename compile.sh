322700b1b061756814ce52b0523ffee1afea7833
skylens <skylens116@outlook.com>
2021-07-04 13:56:06 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xcodebuild clean -workspace /Users/runner/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -configuration Release
-xcodebuild archive -workspace /Users/runner/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -archivePath /Users/runner/archive/ShadowsocksX-NG.xcarchive
+xcodebuild clean -workspace /Users/runner/work/ShadowsocksX-NG/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -configuration Release
+xcodebuild archive -workspace /Users/runner/work/ShadowsocksX-NG/ShadowsocksX-NG/ShadowsocksX-NG.xcworkspace -scheme ShadowsocksX-NG -archivePath /Users/runner/archive/ShadowsocksX-NG.xcarchive
 cd /Users/runner/archive/ShadowsocksX-NG.xcarchive/Products/Applications
 tar -zcvf /Users/runner/ShadowsocksX-NG-`git describe --tags`.tgz ShadowsocksX-NG.app
